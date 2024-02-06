d54e76fef3d52cd14e9010a2b9708c8b459f48dd
Joshua Cantu <joshua@thecantu.com>
2024-02-05 22:06:14 -0600

    Added UserDtos

--- Selu383.SP24.Api/Features/UserRole/UserRoleDtos.cs
+++ Selu383.SP24.Api/Features/UserRole/UserRoleDtos.cs
@@ -1,6 +1,26 @@
 ﻿namespace Selu383.SP24.Api.Features.UserRole
 {
-    public class UserRoleDtos
+    public class UserCreateDto
     {
+        public string UserName { get; set; }
+        public string[] Roles { get; set; }
+        public string Password { get; set; }
     }
+
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public string[] Roles { get; set; }
+
+    }
+
+
 }
