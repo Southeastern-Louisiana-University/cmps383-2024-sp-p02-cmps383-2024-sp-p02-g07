5e826f30a59f76cfc3033c62b7109a6a5ea5a26a
Joshua Cantu <joshua@thecantu.com>
2024-02-05 22:10:01 -0600

    Moved dtos to correct folder

--- Selu383.SP24.Api/Features/UserRole/UserRoleDtos.cs
+++ Selu383.SP24.Api/Features/UserRole/UserRoleDtos.cs
@@ -1,26 +1,6 @@
 ﻿namespace Selu383.SP24.Api.Features.UserRole
 {
-    public class UserCreateDto
+    public class UserRoleDtos
     {
-        public string UserName { get; set; }
-        public string[] Roles { get; set; }
-        public string Password { get; set; }
     }
-
-    public class UserLoginDto
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-
-    public class UserDto
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-
-        public string[] Roles { get; set; }
-
-    }
-
-
 }
--- Selu383.SP24.Api/Features/Users/UserDtos.cs
+++ Selu383.SP24.Api/Features/Users/UserDtos.cs
@@ -1,6 +1,27 @@
-﻿namespace Selu383.SP24.Api.Features.Users
+﻿namespace Selu383.SP24.Api.Features.UserRole
 {
-    public class UserDtos
+    public class UserCreateDto
     {
+        public string UserName { get; set; }
+        public string[] Roles { get; set; }
+        public string Password { get; set; }
     }
+
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public string[] Roles { get; set; }
+
+    }
+
+
 }
+
