a720dab06391ee460eede2fb827b7aee7c76b54d
Joshua Cantu <joshua@thecantu.com>
2024-02-12 00:08:16 -0600

    Trying to link users roles and userroles

--- Selu383.SP24.Api/Data/DataContext.cs
+++ Selu383.SP24.Api/Data/DataContext.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Selu383.SP24.Api.Features.UserRoles;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Selu383.SP24.Api.Features.Users;
+using Selu383.SP24.Api.Features.Roles;
+using Selu383.SP24.Api.Features.Hotels;
 
 namespace Selu383.SP24.Api.Data;
 
@@ -17,5 +23,17 @@ public class DataContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+
+        var userRoleBuilder = modelBuilder.Entity<UserRole>();
+
+        userRoleBuilder.HasKey(x => new { x.UserId, x.RoleId });
+
+        userRoleBuilder.HasOne(x => x.Role)
+            .WithMany(x => x.Users)
+            .HasForeignKey(x => x.RoleId);
+
+        userRoleBuilder.HasOne(x => x.User)
+            .WithMany(x => x.Roles)
+            .HasForeignKey(x => x.UserId);
     }
 }
