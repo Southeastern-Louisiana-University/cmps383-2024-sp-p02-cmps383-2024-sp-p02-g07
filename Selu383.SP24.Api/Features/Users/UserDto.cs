7c432edee3e3297ff7f683b0d3bc95fe922dd3c1
Joshua Cantu <joshua@thecantu.com>
2024-02-13 21:33:22 -0600

    Added all User Dtos

--- Selu383.SP24.Api/Features/Users/UserDto.cs
+++ Selu383.SP24.Api/Features/Users/UserDto.cs
@@ -7,4 +7,17 @@
         public string[] Roles { get; set; }
 
     }
+
+    public class UserCreateDto
+    {
+        public string UserName { get; set; }
+        public string[] Roles { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 }
